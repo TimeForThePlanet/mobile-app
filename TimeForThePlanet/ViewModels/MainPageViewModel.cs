d1fb4bb0ab2b5563cd3fcf71d48bbccb0835318b
tuxtoby <thomas.impery@gmail.com>
2021-07-29 21:28:16 +0200

    ✨ Add Galaxy of Action page

---
+++ TimeForThePlanet/ViewModels/MainPageViewModel.cs
@@ -0,0 +1,19 @@
+﻿using System.Windows.Input;
+using Xamarin.Forms;
+
+namespace TimeForThePlanet.ViewModels
+{
+    class MainPageViewModel
+    {
+        public ICommand GoToGalaxyOfActionCommand { get; }
+
+        public MainPageViewModel()
+        {
+            GoToGalaxyOfActionCommand = new Command(GoToGalaxyOfAction);
+        }
+
+        public void GoToGalaxyOfAction()
+        {
+        }
+    }
+}
---
+++ TimeForThePlanet/Views/MainPage.xaml.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Xamarin.Forms;
+
+namespace TimeForThePlanet.Views
+{
+    public partial class MainPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_OnClicked(object sender, EventArgs e)
+        {
+            var page = new GalaxyOfAction();
+            Application.Current.MainPage.Navigation.PushAsync(page);
+        }
+    }
+}
