a66974201b29bd9c7a292a564acb648733c47c71
tuxtoby <thomas.impery@gmail.com>
2021-11-13 10:17:25 +0100

    🔥 Remove useless icon

--- TimeForThePlanet.Android/MainActivity.cs
+++ TimeForThePlanet.Android/MainActivity.cs
@@ -8,7 +8,7 @@ using Xamarin.Forms.PlatformConfiguration.AndroidSpecific;
 
 namespace TimeForThePlanet.Droid
 {
-    [Activity(Label = "TimeForThePlanet", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize)]
+    [Activity(Label = "TimeForThePlanet", Icon = "@mipmap/time", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize)]
     public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
     {
         protected override void OnCreate(Bundle savedInstanceState)
