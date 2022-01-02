8a41b954f15c88c581b2aeae87eb84a72dd94671
tuxtoby <thomas.impery@gmail.com>
2022-01-02 17:26:02 +0100

    ♻️ Use component for home page

---
+++ TimeForThePlanet/Views/UIComponents/HomeIcon.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System.Windows.Input;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace TimeForThePlanet.Views.Components
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class HomeIcon : Grid
+    {
+        public HomeIcon()
+        {
+            InitializeComponent();
+        }
+
+        public static readonly BindableProperty ImageSrcProperty =
+            BindableProperty.Create(nameof(ImageSrc), typeof(string), typeof(HomeIcon));
+
+        public static readonly BindableProperty TextProperty =
+            BindableProperty.Create(nameof(Text), typeof(string), typeof(HomeIcon));
+
+        public static readonly BindableProperty TappedCommandProperty =
+            BindableProperty.Create(nameof(TappedCommand), typeof(ICommand), typeof(HomeIcon));
+
+        public string ImageSrc
+        {
+            get => (string)GetValue(ImageSrcProperty);
+            set => SetValue(ImageSrcProperty, value);
+        }
+
+        public string Text
+        {
+            get => (string)GetValue(TextProperty);
+            set => SetValue(TextProperty, value);
+        }
+
+        public ICommand TappedCommand
+        {
+            get => (ICommand)GetValue(TappedCommandProperty);
+            set => SetValue(TappedCommandProperty, value);
+        }
+
+    }
+}
